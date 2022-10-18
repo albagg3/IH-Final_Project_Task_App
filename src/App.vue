00c596be5bec693b25825d2ba515a85771e6b102
Alba Garcia <garciagonzalezalba@gmail.com>
2022-10-18 12:51:39 +0200

    añadidas las variables de entorno de supabase

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,10 @@
   <router-view></router-view>
 </template>
 <script setup>
+import {createClient} from '@supabase/supabase-js'
+
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY)
+console.log(supabase)
 console.log(process.env.NODE_ENV, import.meta.env.BASE_URL)
 </script>
 <style scoped>
